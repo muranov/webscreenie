c5a8ebe254fe75a7e9998e53be7af8d9a18ca896
ducharme.mathieu <ducharme.mathieu@8c8428f0-de4e-0410-9f94-f34973fbe4fb>
2008-06-05 03:20:26 +0000

    Implements offset. Use --offset 100x100 for example

--- trunk/src/webscreenie.cpp
+++ trunk/src/webscreenie.cpp
@@ -24,12 +24,14 @@ http://code.google.com/p/wkhtmltopdf/
 
 #include <string.h>
 
+#include <QtDebug>
 #include <QtGui>
-#include <QString>
+
 #include <QImage>
-#include <QWebView>
-#include <QWebPage>
+#include <QString>
 #include <QWebFrame>
+#include <QWebPage>
+#include <QWebView>
 
 QApplication * app;
 
@@ -39,30 +41,31 @@ QApplication * app;
 void WebScreenie::usage(FILE * fd) 
 {
 	fprintf(fd,
-"Usage: webscreenie [OPTIONS]... [<input file> [<output file>]]\n"
+"Usage: webscreenie [OPTIONS]... [<input url> [<output file>]]\n"
 "\n"
 "Converts a html page to an image\n"
 "Use without options for the graphical user interface\n"
 "\n"
+"Example:\n"
+"webscreenie --url http://www.example.com --file example.png --target_size 640x480\n"
+"\n"
 "Options:\n"
-"  -?, --help                 show this help / usage message\n"
-"  -u, --url <url>            url to open\n"
-"  -f, --file <url>         use url as output\n"
-"  -o, --options <file>       configuration file\n" 
+"  --help                 show this help / usage message\n"
+"  --url <url>            url to open\n"
+"  --file <url>           use url as output\n"
 "\n"
 "Image Options:\n"
-"  -x, --offset_x <pixels>    offset from top to crop\n"
-"  -y, --offset_y <pixels>    offset from left to crop\n"
-"  -w, --width <pixels>       width of the image to grab\n"
-"  -h, --height <pixels>      height of the image to grab\n"
-"  -r, --ratio <percentage>   scale ratio (ex: 0.5)\n"
+"  --offset <WxY>         offset from upper-top to crop\n"
+"  --size <WxY>           dimension of the web page capture\n"
+"  --target_size <WxY>    target dimension of the output image\n"
 "\n"
 "Advanced Options:\n"
-"  -s, --script <script>      custom javascript to execute before render\n"
-"      --script_file <file>   custom javascript file to execute before render\n"
-"  -c, --css <css>            custom CSS to change render style\n"
-"      --css_file <file>      custom CSS\n"
-"  -d, --delay <seconds>      delay, in seconds, before render\n"
+"  --script <script>      custom javascript to execute before render\n"
+"  --script_file <file>   custom javascript file to execute before render\n"
+"  --css <css>            custom CSS to change render style\n"
+"  --css_file <file>      custom CSS\n"
+"  --delay <ms>           delay, in microseconds, before render\n"
+"  --options <file>       configuration file\n" 
 	);
 }
 
@@ -71,86 +74,64 @@ void WebScreenie::usage(FILE * fd)
 */
 void WebScreenie::parseArgs(int argc, const char ** argv)
 {
-	
-	//Load default configuration
-	in = "http://www.example.com";
-	out = "example.png";
-	
-	int x=0;
 	for(int i=1; i < argc; ++i) {
 
-		if(argv[i][0] != '-') {
-			//Default arguments
-			++x;
-			if(x==1) {
-				in = argv[i];
-			}
-			else if(x == 2) {
-				out = argv[i];
-			}
-			else {
-				usage(stderr);
-				exit(1);
-			}
-			continue;
-		}
-		
 		if(argv[i][1] == '-') { //Long style arguments
 			if(!strcmp(argv[i],"--help")) {
 				usage(stdout); 
 				exit(0);
 			}
-			else if(!strcmp(argv[i],"--input")) {
+			else if(!strcmp(argv[i],"--url")) {
 				if(i+1>= argc) {
 					usage(stderr);
 					exit(1);
 				}
-				in = argv[++i];
+				url = argv[++i];
 			} 
-			else if(!strcmp(argv[i],"--output")) {
+			else if(!strcmp(argv[i],"--file")) {
 				if(i+1>= argc) {
 					usage(stderr);
 					exit(1);
 				}
-				out = argv[++i];
-			} 
+				file = argv[++i];
+			}
+			else if(!strcmp(argv[i],"--offset")) {
+				if(i+1>= argc) {
+					usage(stderr);
+					exit(1);
+				}
+				QString arg = argv[++i];
+				QRegExp regex("(\\d+)x(\\d+)");
+				int pos = regex.indexIn(arg);
+				if(pos > -1) {
+					options.offset.setX(regex.cap(1).toInt());
+					options.offset.setY(regex.cap(2).toInt());
+				}
+				else {
+					fprintf(stderr, "Invalid offset parameter: %s", arg.toAscii().constData());
+					usage(stderr);
+					exit(1);
+				}
+			}
+			else if(!strcmp(argv[i],"--size")) {
+				if(i+1>= argc) {
+					usage(stderr);
+					exit(1);
+				}
+				;
+				
+			}
+			else if(!strcmp(argv[i],"--target_size")) {
+				if(i+1>= argc) {
+					usage(stderr);
+					exit(1);
+				}
+			}
 			else {
 				usage(stderr);
 				exit(1);
 			}
 			continue;
-		}
-
-		//Short style arguments
-		int c=i;
-		for(int j=1; argv[c][j] != '\0'; ++j) {
-			switch(argv[c][j]) {
-				case 'h':
-					usage(stdout); 
-					exit(0);
-				break;
-	
-				case 'i':
-					if(i+1>= argc) {
-						usage(stderr);
-						exit(1);
-					}
-					in = argv[++i];
-				break;
-	
-				case 'o':
-					if(i+1>= argc) {
-						usage(stderr);
-						exit(1);
-					}
-					out = argv[++i];
-				break;
-	
-				default:
-					usage(stderr);
-					exit(1);
-				break;
-			}
 		}
 	}
 }
@@ -160,18 +141,27 @@ void WebScreenie::parseArgs(int argc, const char ** argv)
 */
 void WebScreenie::run(int argc, const char ** argv)
 {
-	//Parse the arguments
-	parseArgs(argc,argv);
-	//Make a url of the input file
-	QUrl url(in);
+	// Set the default options
+	options.offset.setX(0);
+	options.offset.setY(0);
 	
-	//When loading is progressing we want loadProgress to be called
+	options.size.setWidth(0);
+	options.size.setHeight(0);
+	
+	options.targetSize.setWidth(0);
+	options.targetSize.setHeight(0);
+	
+	options.delay = 0;
+	
+	url = "http://www.example.com";
+	file = "webscreenie.png";
+
+	parseArgs(argc,argv);
+	
 	connect(&webview, SIGNAL(loadProgress(int)), this, SLOT(loadProgress(int)));
-	//Once the loading is done we want loadFinished to be called
 	connect(&webview, SIGNAL(loadFinished(bool)), this, SLOT(loadFinished(bool)));
 	
-	//Tell the vebview to load for the newly created url
-	webview.load(url);
+	load(url);
 }
 
 /*
@@ -189,29 +179,68 @@ void WebScreenie::loadFinished(bool success)
 		return;
 	}
 	
+	render();
+	
+	app->quit();
+}
+/*
+*
+*/
+void WebScreenie::load(QUrl url)
+{
+	webview.load(url);
+}
+
+/*
+*
+*/
+void WebScreenie::render()
+{
 	// Maximize content
 	webview.page()->setViewportSize(webview.page()->mainFrame()->contentsSize());
-	QImage image(webview.page()->viewportSize(), QImage::Format_ARGB32);
-	QPainter painter(&image);
+	
+	// Hide scrollbars
+	webview.page()->mainFrame()->setScrollBarPolicy(Qt::Horizontal, Qt::ScrollBarAlwaysOff);
+	webview.page()->mainFrame()->setScrollBarPolicy(Qt::Vertical, Qt::ScrollBarAlwaysOff);
 	
 	// TODO: Read from command-line / UI options
-	QString script;
-	script = "var links = document.getElementsByTagName('a');for(var i = 0; i < links.length; i++) {links[i].style.border = '2px solid red';}";
+	options.script = ""
+	"var links = document.getElementsByTagName('a');"
+	"for(var i = 0; i < links.length; i++) {"
+	"	links[i].style.border = '3px solid blue';"
+	"}";
 	
-	if(!script.isEmpty()) {
-		QVariant result;
-		result = webview.page()->mainFrame()->evaluateJavaScript(script);
-		//qDebug() << result;
+	if(!options.script.isEmpty()) {
+		QVariant result = webview.page()->mainFrame()->evaluateJavaScript(options.script);
+		qDebug() << result.toString();
 	}
+	
+	if(!options.css.isEmpty()) {
+		
+	}
+	
+	int x = options.offset.x();
+	int y = options.offset.y();
+	int w = webview.page()->viewportSize().width() - x;
+	int h = webview.page()->viewportSize().height() - y;
+
+	QImage image(webview.page()->viewportSize(), QImage::Format_ARGB32);
+	QPainter painter(&image);
 	
 	webview.page()->mainFrame()->render(&painter);
 	painter.end();
 	
-	image.save(out);
+	// Crop
+	QImage cropped = image.copy(x,y,w,h);
 	
-	printf("Image successfully saved to \"%s\"\n", out);
+	// Resize
+	//QImage resized = image.scaled();
 	
-	app->quit();
+	cropped.save(file);
+	
+	
+	
+	printf("Image successfully saved to \"%s\"\n", file.toAscii().constData());
 }
 
 /*
@@ -220,7 +249,7 @@ void WebScreenie::loadFinished(bool success)
 void WebScreenie::loadProgress(int progress)
 {
 	//Print out the load status
-	printf("Loading page \"%s\": %d%%   \r", in, progress);
+	printf("Loading page \"%s\": %d%%   \r", url.toString().toAscii().constData(), progress);
 	fflush(stdout);
 }
 
